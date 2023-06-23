ff4c71a01b2783928a971ef8d6ff3a0bf110160d
unknown <syc1488@naver.com>
2023-06-23 15:07:19 +0900

    added refresh function to the ImageButton

--- app/src/main/java/com/shin/dnfauctionwidget/AuctionWidget.java
+++ app/src/main/java/com/shin/dnfauctionwidget/AuctionWidget.java
@@ -1,8 +1,11 @@
 package com.shin.dnfauctionwidget;
 
+import android.app.PendingIntent;
 import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
+import android.content.ComponentName;
 import android.content.Context;
+import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.drawable.Drawable;
 import android.widget.RemoteViews;
@@ -24,12 +27,6 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import java.net.HttpURLConnection;
-import java.net.URL;
-
-/**
- * Implementation of App Widget functionality.
- */
 public class AuctionWidget extends AppWidgetProvider {
 
     static void updateAppWidget(Context context, AppWidgetManager appWidgetManager, int appWidgetId) {
@@ -44,99 +41,25 @@ public class AuctionWidget extends AppWidgetProvider {
             // 위젯 레이아웃 설정
             RemoteViews remoteViews = new RemoteViews(context.getPackageName(), R.layout.auction_widget);
 
+            // ImageButton에 클릭 리스너 설정
+            Intent intent = new Intent(context, AuctionWidget.class);
+            intent.setAction("Refresh");
+            intent.putExtra(AppWidgetManager.EXTRA_APPWIDGET_ID, appWidgetId);
+            PendingIntent pendingIntent = PendingIntent.getBroadcast(context, 0, intent, 0);
+            remoteViews.setOnClickPendingIntent(R.id.refresh_button, pendingIntent);
 
             String itemName = "균열의 단편";
-            String apiKey = context.getString(R.string.api_Key);
 
             //이미지 삽입
             remoteViews.setImageViewResource(R.id.gold1, R.drawable.gold);
             remoteViews.setImageViewResource(R.id.gold2, R.drawable.gold);
             remoteViews.setTextViewText(R.id.item_name, itemName);
 
-            RequestQueue requestQueue = Volley.newRequestQueue(context);
-            // API 요청
-            String requestURL1 = "https://api.neople.co.kr/df/items?itemName=";
-            String finalURL1 = requestURL1 + itemName + "&apikey=" + apiKey;
-            System.out.println(finalURL1);
-            JsonObjectRequest jsonObjectRequest1 = new JsonObjectRequest(Request.Method.GET, finalURL1, null,
-                    new Response.Listener<JSONObject>() {
-                        @Override
-                        public void onResponse(JSONObject response) {
-                            try {
-                                // JSON 데이터 파싱
-                                JSONArray rowsArray = response.getJSONArray("rows");
-                                JSONObject firstRow = rowsArray.getJSONObject(0);
+            //api 요청
+            performApiRequest(context, appWidgetId);
 
-                                String itemID = firstRow.getString("itemId");
-
-                                // Glide를 사용하여 이미지 로드
-                                Glide.with(context)
-                                        .asBitmap()
-                                        .load("https://img-api.neople.co.kr/df/items/" + itemID)
-                                        .into(new CustomTarget<Bitmap>() {
-                                            @Override
-                                            public void onResourceReady(@NonNull Bitmap resource, @Nullable Transition<? super Bitmap> transition) {
-                                                // 비트맵 이미지를 RemoteViews에 설정
-                                                remoteViews.setImageViewBitmap(R.id.item_image, resource);
-
-                                                // 위젯 업데이트
-                                                appWidgetManager.updateAppWidget(appWidgetId, remoteViews);
-                                            }
-
-                                            @Override
-                                            public void onLoadCleared(@Nullable Drawable placeholder) {
-                                                // 이미지 로드가 취소되거나 삭제된 경우 처리할 내용
-                                            }
-                                        });
-                            } catch (JSONException e) {
-                                e.printStackTrace();
-                                Toast.makeText(context, "JSON Parsing Error", Toast.LENGTH_SHORT).show();
-                            }
-                        }
-                    },
-                    new Response.ErrorListener() {
-                        @Override
-                        public void onErrorResponse(VolleyError error) {
-                            Toast.makeText(context, "API Request Error", Toast.LENGTH_SHORT).show();
-                        }
-                    });
-
-            String requestURL2 = "https://api.neople.co.kr/df/auction?&limit=400&sort=unitPrice:asc&itemName=";
-            String finalURL2 = requestURL2 + itemName + "&apikey=" + apiKey;
-            JsonObjectRequest jsonObjectRequest2 = new JsonObjectRequest(Request.Method.GET, finalURL2, null,
-                    new Response.Listener<JSONObject>() {
-                        @Override
-                        public void onResponse(JSONObject response) {
-                            try {
-                                // JSON 데이터 파싱
-                                JSONArray rowsArray = response.getJSONArray("rows");
-                                JSONObject firstRow = rowsArray.getJSONObject(0);
-
-                                Integer unitPrice = firstRow.getInt("unitPrice");
-                                Integer avgPrice = firstRow.getInt("averagePrice");
-
-                                // 위젯에 텍스트 설정
-                                remoteViews.setTextViewText(R.id.auction_minPrice, unitPrice.toString());
-                                remoteViews.setTextViewText(R.id.auction_avgPrice, avgPrice.toString());
-
-                                // 위젯 업데이트
-                                appWidgetManager.updateAppWidget(appWidgetId, remoteViews);
-                            } catch (JSONException e) {
-                                e.printStackTrace();
-                                Toast.makeText(context, "JSON Parsing Error", Toast.LENGTH_SHORT).show();
-                            }
-                        }
-                    },
-                    new Response.ErrorListener() {
-                        @Override
-                        public void onErrorResponse(VolleyError error) {
-                            Toast.makeText(context, "API Request Error", Toast.LENGTH_SHORT).show();
-                        }
-                    });
-
-            // API 요청 추가
-            requestQueue.add(jsonObjectRequest1);
-            requestQueue.add(jsonObjectRequest2);
+            // 위젯 업데이트
+            appWidgetManager.updateAppWidget(appWidgetId, remoteViews);
         }
     }
 
@@ -150,4 +73,107 @@ public class AuctionWidget extends AppWidgetProvider {
         // Enter relevant functionality for when the last widget is disabled
     }
 
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        super.onReceive(context, intent);
+
+        if (intent.getAction().equals("Refresh")) { // 액션 이름에 따라 처리
+            // ImageButton가 클릭되었을 때 실행할 함수 호출
+            int appWidgetId = intent.getIntExtra(AppWidgetManager.EXTRA_APPWIDGET_ID, AppWidgetManager.INVALID_APPWIDGET_ID);
+            if (appWidgetId != AppWidgetManager.INVALID_APPWIDGET_ID) {
+                performApiRequest(context, appWidgetId);
+            }
+        }
+    }
+
+    private void performApiRequest(Context context, int appWidgetId) {
+        String itemName = "균열의 단편";
+        String apiKey = context.getString(R.string.api_Key);
+
+        // API 요청
+        String requestURL1 = "https://api.neople.co.kr/df/items?itemName=";
+        String finalURL1 = requestURL1 + itemName + "&apikey=" + apiKey;
+        JsonObjectRequest jsonObjectRequest1 = new JsonObjectRequest(Request.Method.GET, finalURL1, null,
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        try {
+                            // JSON 데이터 파싱
+                            JSONArray rowsArray = response.getJSONArray("rows");
+                            JSONObject firstRow = rowsArray.getJSONObject(0);
+
+                            String itemID = firstRow.getString("itemId");
+
+                            // Glide를 사용하여 이미지 로드
+                            Glide.with(context)
+                                    .asBitmap()
+                                    .load("https://img-api.neople.co.kr/df/items/" + itemID)
+                                    .into(new CustomTarget<Bitmap>() {
+                                        @Override
+                                        public void onResourceReady(@NonNull Bitmap resource, @Nullable Transition<? super Bitmap> transition) {
+                                            // 비트맵 이미지를 RemoteViews에 설정
+                                            RemoteViews remoteViews = new RemoteViews(context.getPackageName(), R.layout.auction_widget);
+                                            remoteViews.setImageViewBitmap(R.id.item_image, resource);
+
+                                            // 위젯 업데이트
+                                            AppWidgetManager appWidgetManager = AppWidgetManager.getInstance(context);
+                                            appWidgetManager.updateAppWidget(appWidgetId, remoteViews);
+                                        }
+
+                                        @Override
+                                        public void onLoadCleared(@Nullable Drawable placeholder) {
+                                            // 이미지 로드가 취소되거나 삭제된 경우 처리할 내용
+                                        }
+                                    });
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                            Toast.makeText(context, "JSON Parsing Error", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Toast.makeText(context, "API Request Error", Toast.LENGTH_SHORT).show();
+                    }
+                });
+
+        String requestURL2 = "https://api.neople.co.kr/df/auction?&limit=400&sort=unitPrice:asc&itemName=";
+        String finalURL2 = requestURL2 + itemName + "&apikey=" + apiKey;
+        JsonObjectRequest jsonObjectRequest2 = new JsonObjectRequest(Request.Method.GET, finalURL2, null,
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        try {
+                            // JSON 데이터 파싱
+                            JSONArray rowsArray = response.getJSONArray("rows");
+                            JSONObject firstRow = rowsArray.getJSONObject(0);
+
+                            Integer unitPrice = firstRow.getInt("unitPrice");
+                            Integer avgPrice = firstRow.getInt("averagePrice");
+
+                            // 위젯에 텍스트 설정
+                            RemoteViews remoteViews = new RemoteViews(context.getPackageName(), R.layout.auction_widget);
+                            remoteViews.setTextViewText(R.id.auction_minPrice, unitPrice.toString());
+                            remoteViews.setTextViewText(R.id.auction_avgPrice, avgPrice.toString());
+
+                            // 위젯 업데이트
+                            AppWidgetManager appWidgetManager = AppWidgetManager.getInstance(context);
+                            appWidgetManager.updateAppWidget(appWidgetId, remoteViews);
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                            Toast.makeText(context, "JSON Parsing Error", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Toast.makeText(context, "API Request Error", Toast.LENGTH_SHORT).show();
+                    }
+                });
+        RequestQueue requestQueue = Volley.newRequestQueue(context);
+        requestQueue.add(jsonObjectRequest1);
+        requestQueue.add(jsonObjectRequest2);
+    }
 }
