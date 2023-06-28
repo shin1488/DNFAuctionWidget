9803f3e3ba571b9a5016c3b1e1ea0abb8c8d864d
unknown <syc1488@naver.com>
2023-06-28 21:08:55 +0900

    added open api link in neople logo

--- app/src/main/java/com/shin/dnfauctionwidget/MainActivity.java
+++ app/src/main/java/com/shin/dnfauctionwidget/MainActivity.java
@@ -1,8 +1,10 @@
 package com.shin.dnfauctionwidget;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.KeyEvent;
+import android.view.View;
 import android.view.inputmethod.EditorInfo;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -47,6 +49,17 @@ public class MainActivity extends AppCompatActivity {
                     return true;
                 }
                 return false;
+            }
+        });
+
+        binding.neopleLogo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Uri webpage = Uri.parse("https://developers.neople.co.kr");
+                Intent neopleOpenApi = new Intent(Intent.ACTION_VIEW, webpage);
+                if (neopleOpenApi.resolveActivity(getPackageManager()) != null) {
+                    startActivity(neopleOpenApi);
+                }
             }
         });
     }
